9b23e0784a5058d9bf620eef30f87ef0b1f4844d
Epoupa Nyadjroh <epoupaw@gmail.com>
2025-11-09 02:45:03 +0100

    Made some modifications to chat_data script. Takes args from command line

--- utils/chat_data.py
+++ utils/chat_data.py
@@ -1,4 +1,5 @@
 """This module is used to get chats from the platform and extract user ids from the chats"""
+import argparse
 import json
 import sys
 import os
@@ -178,31 +179,84 @@ def get_chat_data(chat):
             data_item['body'] = chat[type_of_chat]['body']
     return data_item
 
-def export_data_to_csv(data):
+def export_data_to_csv(chat_data, filename=None):
     """Exports data to a csv file
 
     Args:
         data (list): a list of chats
     """
-    file_exist = os.path.exists('chat_bot_data.csv')
-    with open('chat_bot_data.csv', 'a' , encoding='utf-8', newline='') as csv_file:
+    if not filename:
+        filename = 'chat_bot_data.csv'
+    
+    file_exist = os.path.exists(filename)
+    with open(filename, 'a' , encoding='utf-8', newline='') as csv_file:
         fields = ['id', 'from', 'type', 'source', 'status', 'body', 'timestamp']
         csv_writer =  csv.DictWriter(csv_file, fieldnames=fields)
         if not file_exist:
             csv_writer.writeheader()
-        for item in data:
+        for item in chat_data:
             csv_writer.writerow(item)
         print('Writing Data Done')
     
 
 if __name__ == '__main__':
-    from_timestamp = 1751328000
-    to_timestamp = from_timestamp + 86400
-    for day in range(0, (NUMBER_OF_DAYS[Months.MAY.value] - 1)):
-        print(f'Getting chats from {datetime.fromtimestamp(from_timestamp).strftime('%a %d %b %Y, %I:%M%p')} to {datetime.fromtimestamp(to_timestamp).strftime('%a %d %b %Y, %I:%M%p')}')
-        chats = getChats(COUNTS, from_timestamp, to_timestamp)
+    import argparse
+    import re
+
+    parser = argparse.ArgumentParser(description='Get chats from the platform and extract user data from the chats'
+                                        ' and export to a csv file')
+    parser.add_argument('--count', "-c", type=int, default=COUNTS, help='Number of chats to get', required=False)
+    parser.add_argument('--start', "-s", type=str,  help='From timestamp. Should be in the format YYYY-MM-DD HH:MM', default=date.fromtimestamp(INITIAL_TIME).strftime('%Y-%m-%d %H:%M'))
+    parser.add_argument('--stop', "-e", type=str,  help='To timestamp. Should be in the format YYYY-MM-DD HH:MM', default=date.fromtimestamp(0).strftime('%Y-%m-%d'))
+    parser.add_argument('--export', "-x", type=str,  help='Filename to export data to', default='chat_bot_data.csv', required=False)
+
+    args = parser.parse_args()
+
+    count = args.count
+    
+    if count <= 0:
+        print('Error: Count should be greater than 0')
+        sys.exit(1)
+    
+    start_time = args.start
+    if not start_time:
+        start_time = input('Start time (YYYY-MM-DD HH:MM): ').strip()
+        
+    if not re.match(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}$', start_time):
+        print('Error: Start time should be in the format YYYY-MM-DD HH:MM')
+        sys.exit(1)
+    
+    stop_time = args.stop
+    if not stop_time:
+        stop_time = input('Stop time (YYYY-MM-DD HH:MM): ').strip()
+    if not re.match(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}$', stop_time):
+        print('Error: Stop time should be in the format YYYY-MM-DD HH:MM')
+        sys.exit(1)
+    
+    export_filename = args.export
+    
+    #start_month = int(re.search(r'\-(\d{2})\-', start_time).group(1))
+    #end_month = int(re.search(r'\-(\d{2})\-', stop_time).group(1))
+    from_timestamp = int(datetime.strptime(start_time, '%Y-%m-%d %H:%M').timestamp())
+    to_timestamp = int(datetime.strptime(stop_time, '%Y-%m-%d %H:%M').timestamp())
+    
+    #verify if from_timestamp is less than to_timestamp
+    if from_timestamp >= to_timestamp:
+        print('Error: From timestamp should be less than To timestamp')
+        sys.exit(1)
+    
+    stop = 0
+    print(f'Exporting data to {export_filename}')
+    
+    while from_timestamp < to_timestamp:
+        if to_timestamp - from_timestamp < 86400:
+            stop = to_timestamp
+        else:
+            stop = from_timestamp + 86400
+        print(f'Getting chats from {datetime.fromtimestamp(from_timestamp).strftime("%a %d %b %Y, %I:%M%p")} to {datetime.fromtimestamp(stop).strftime("%a %d %b %Y, %I:%M%p")}')
+        chats = getChats(COUNTS, from_timestamp, stop)
         data = get_users_data(chats)
-        export_data_to_csv(data)
-        from_timestamp = to_timestamp
-        to_timestamp = from_timestamp + 86400
+        export_data_to_csv(data, filename=export_filename)
+        from_timestamp = stop
+
     print('Done')
